6a39c427b424482309ab7fc114d1d8a8cda3b857
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-05-13 11:58:53 -0400

    button to toggle importance

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -8,15 +8,12 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect');
     axios
       .get('http://localhost:3001/notes')
       .then(response => {
-        console.log('promise fulfilled');
         setNotes(response.data)
       })
   }, [])
-  console.log('render', notes.length, 'notes');
 
   const addNote = (event) => {
     event.preventDefault()
@@ -30,27 +27,43 @@ const App = (props) => {
     axios
       .post('http://localhost:3001/notes', noteObject)
       .then(response => {
-        console.log(response);
+        /* the newnote returned by backend server is added to the list of notes
+        in the application state to trigger a browser re-render */
+        setNotes(notes.concat(response.data))
+        setNewNote('')
       })
   }
 
   const handleNoteChange = (event) => {
-    console.log(event.target.value);
     setNewNote(event.target.value)
   }
-  /*
-  the HTML input element needs an onChange handler to allow the user to
+  /* the HTML input element needs an onChange handler to allow the user to
   change the element. Only setting value=newNote state causes the App component
-  to take control of the element, preventing input.
-  */
+  to take control of the element, preventing input. */
 
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important === true)
-  /*
-  a ternary operator that changes notesToShow depending on whether showAll is T/F.
-  Using notesToShow allows for conditional filtering of displayed note elements.
-  */
+  /* a ternary operator that changes notesToShow depending on whether showAll is T/F.
+  Using notesToShow allows for conditional filtering of displayed note elements. */
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+    /* Create a new note that is a copy of the old note except for the important
+    property. We create a new note instead of mutating the note directly as that
+    would be mutating state directly */
+
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+        //replace Notes with a new array that is the same except for the changed note
+      })
+  }
+  /* an event handler function passed to the Note component to allow buttons to
+  toggle note importance attribute */
 
   return (
     <div>
@@ -62,7 +75,10 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />  
+          <Note key={note.id} note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+          //create a Note element for every note in notesToShow  
         )}
       </ul>
       <form onSubmit={addNote}>
