a16d18da413f3f7cf999246b851c3cc36fd82ee9
burneverything1 <63659097+burneverything1@users.noreply.github.com>
2021-05-12 22:56:13 -0400

    load with react effect hook

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')        //a state for storing user-submitted input
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes');
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5
     }
 
-    setNotes(notes.concat(noteObject))    //concat creates a new copy, never mutate state directly
-    setNewNote('')
+    //send created note in POST request
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response);
+      })
   }
 
   const handleNoteChange = (event) => {
